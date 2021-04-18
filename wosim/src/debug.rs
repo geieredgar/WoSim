e5210bbf84b5430fb1bda4d5cdab3a0e38c106d2
Edgar Geier <egeier@rhrk.uni-kl.de>
2021-04-18 12:47:58 +0200

    Improve debug UI

--- wosim/src/debug.rs
+++ wosim/src/debug.rs
@@ -8,7 +8,7 @@ use egui::{
         plot::{Curve, Plot, Value},
         Slider,
     },
-    CtxRef, Window,
+    Color32, CtxRef, Window,
 };
 
 use crate::renderer::RenderTimestamps;
@@ -21,6 +21,8 @@ pub struct DebugContext {
     frame_start: Instant,
     frame_times: VecDeque<(Instant, Instant, Option<RenderTimestamps>)>,
     frame_times_secs: f64,
+    show_cpu_time: bool,
+    show_gpu_time: bool,
 }
 
 impl DebugContext {
@@ -33,6 +35,8 @@ impl DebugContext {
             frame_start: Instant::now(),
             frame_times: VecDeque::new(),
             frame_times_secs: 10.0,
+            show_cpu_time: true,
+            show_gpu_time: true,
         }
     }
 
@@ -71,30 +75,47 @@ impl DebugContext {
                 ui.label("Frame time storage duration [s]");
                 ui.add(Slider::new(&mut self.frame_times_secs, 0.0f64..=120.0f64));
             });
+            ui.horizontal(|ui| {
+                ui.checkbox(&mut self.show_cpu_time, "Show CPU Time");
+                ui.checkbox(&mut self.show_gpu_time, "Show GPU Time");
+            });
             if let Some(front) = self.frame_times.front() {
                 let origin = front.0;
-                let cpu_curve =
-                    Curve::from_values_iter(self.frame_times.iter().map(|(begin, end, _)| Value {
-                        x: begin.duration_since(origin).as_secs_f64(),
-                        y: end.duration_since(*begin).as_secs_f64() * 1000.0,
-                    }));
-                let gpu_curve = Curve::from_values_iter(self.frame_times.iter().filter_map(
-                    |(begin, _, timestamps)| {
-                        timestamps.as_ref().map(|timestamps| Value {
-                            x: begin.duration_since(origin).as_secs_f64(),
-                            y: timestamps.end - timestamps.begin,
-                        })
-                    },
-                ));
-                ui.add(
-                    Plot::default()
-                        .curve(cpu_curve)
-                        .curve(gpu_curve)
-                        .include_x(0.0)
-                        .include_x(self.frame_times_secs)
-                        .include_y(0.0)
-                        .include_y(50.0),
-                );
+                let plot = Plot::default()
+                    .include_x(0.0)
+                    .include_x(self.frame_times_secs)
+                    .include_y(0.0);
+                let plot = if self.show_cpu_time {
+                    plot.curve(
+                        Curve::from_values_iter(self.frame_times.iter().map(|(begin, end, _)| {
+                            Value {
+                                x: begin.duration_since(origin).as_secs_f64(),
+                                y: end.duration_since(*begin).as_secs_f64() * 1000.0,
+                            }
+                        }))
+                        .color(Color32::RED)
+                        .name("CPU Time"),
+                    )
+                } else {
+                    plot
+                };
+                let plot = if self.show_gpu_time {
+                    plot.curve(
+                        Curve::from_values_iter(self.frame_times.iter().filter_map(
+                            |(begin, _, timestamps)| {
+                                timestamps.as_ref().map(|timestamps| Value {
+                                    x: begin.duration_since(origin).as_secs_f64(),
+                                    y: timestamps.end - timestamps.begin,
+                                })
+                            },
+                        ))
+                        .color(Color32::BLUE)
+                        .name("GPU Time"),
+                    )
+                } else {
+                    plot
+                };
+                ui.add(plot);
             }
         });
     }
