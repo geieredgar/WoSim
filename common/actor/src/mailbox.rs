d05d50c91bb64ce02a4593a853222d9b0708285e
Edgar Geier <egeier@rhrk.uni-kl.de>
2021-04-23 17:15:15 +0200

    Add forward function

--- common/actor/src/mailbox.rs
+++ common/actor/src/mailbox.rs
@@ -10,3 +10,9 @@ pub fn mailbox<T: Send + 'static>() -> (Mailbox<T>, Address<T>) {
     let (send, recv) = unbounded_channel();
     (recv, Address::new(Arc::new(send)))
 }
+
+pub async fn forward<T>(mut mailbox: Mailbox<T>, address: Address<T>) {
+    while let Some(message) = mailbox.recv().await {
+        address.send(message);
+    }
+}
