66cea34ea73ca4922b1ebeb5109c86e218f0a93c
Edgar Geier <egeier@rhrk.uni-kl.de>
2021-06-02 21:09:46 +0200

    Parse command-line using StructOpt in headless bin

--- headless/src/main.rs
+++ headless/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     ffi::CString,
+    net::{Ipv6Addr, SocketAddr, SocketAddrV6},
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
@@ -13,42 +14,64 @@ use error::Error;
 use net::Server;
 use semver::Version;
 use server::Service;
+use structopt::StructOpt;
 use tokio::{runtime::Runtime, time::sleep};
 use util::iterator::MaxOkFilterMap;
 
 mod error;
 mod vulkan;
 
+#[derive(StructOpt)]
+enum Command {
+    Serve {
+        #[structopt(long, short, default_value = "2021")]
+        port: u16,
+    },
+}
+
+impl Command {
+    fn run(self) -> Result<(), Error> {
+        match self {
+            Command::Serve { port } => {
+                let running = Arc::new(AtomicBool::new(true));
+                let r = running.clone();
+                ctrlc::set_handler(move || {
+                    r.store(false, Ordering::SeqCst);
+                })
+                .unwrap();
+                let runtime = Runtime::new()?;
+                runtime.block_on(async {
+                    let version = Version::parse(env!("CARGO_PKG_VERSION")).unwrap();
+                    let instance = Arc::new(Instance::new(
+                        &CString::new("wosim").unwrap(),
+                        version,
+                        vec![],
+                    )?);
+                    let _device = instance
+                        .physical_devices()?
+                        .into_iter()
+                        .max_ok_filter_map(DeviceCandidate::new)?
+                        .ok_or(Error::NoSuitableDeviceFound)?
+                        .create()?;
+                    let service = Arc::new(Service::new().map_err(Error::CreateService)?);
+                    let mut server = Server::new(
+                        service.clone(),
+                        SocketAddr::V6(SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, port, 0, 0)),
+                    );
+                    server.open().map_err(Error::OpenServer)?;
+                    while running.load(Ordering::SeqCst) {
+                        sleep(Duration::from_millis(10)).await;
+                    }
+                    server.close();
+                    let _ = service.stop().await;
+                    Ok(())
+                })
+            }
+        }
+    }
+}
+
 fn main() -> Result<(), Error> {
     env_logger::init();
-    let running = Arc::new(AtomicBool::new(true));
-    let r = running.clone();
-    ctrlc::set_handler(move || {
-        r.store(false, Ordering::SeqCst);
-    })
-    .unwrap();
-    let runtime = Runtime::new()?;
-    runtime.block_on(async {
-        let version = Version::parse(env!("CARGO_PKG_VERSION")).unwrap();
-        let instance = Arc::new(Instance::new(
-            &CString::new("wosim").unwrap(),
-            version,
-            vec![],
-        )?);
-        let _device = instance
-            .physical_devices()?
-            .into_iter()
-            .max_ok_filter_map(DeviceCandidate::new)?
-            .ok_or(Error::NoSuitableDeviceFound)?
-            .create()?;
-        let service = Arc::new(Service::new().map_err(Error::CreateService)?);
-        let mut server = Server::new(service.clone(), "[::]:0".parse().unwrap());
-        server.open().map_err(Error::OpenServer)?;
-        while running.load(Ordering::SeqCst) {
-            sleep(Duration::from_millis(10)).await;
-        }
-        server.close();
-        let _ = service.stop().await;
-        Ok(())
-    })
+    Command::from_args().run()
 }
