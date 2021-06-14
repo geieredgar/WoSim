4d86abdbbd31683c457c47a03a2d8dcbaa323c2a
Edgar Geier <egeier@rhrk.uni-kl.de>
2021-06-14 14:08:19 +0200

    test: mock nuxt-content component

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import { config } from '@vue/test-utils'
+import NuxtContent from './mocks/NuxtContent.vue'
 
 Vue.use(Vuetify)
+config.stubs['nuxt-content'] = NuxtContent
