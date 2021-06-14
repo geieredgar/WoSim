2639e4147e0c617004797637f36b98b1e50f78e7
Edgar Geier <egeier@rhrk.uni-kl.de>
2021-06-14 03:21:30 +0200

    ci: don't update latest.json for pre-releases

--- server/release.js
+++ server/release.js
@@ -19,31 +19,35 @@ writeFileSync('notes.md', notes)
 writeFileSync('content/releases/${tag}.md', `---\ntitle: ${tag}\n---\n${notes}`)
 execSync(`gh release create ${tag} -t ${tag} -F notes.md ${preRelease ? '-p' : ''} release/*`)
 
-const latest = {
-    version: process.env.INPUT_VERSION,
-    notes,
-    pub_date: new Date().toISOString(),
-    platforms: {
-        darwin: {
-            signature:
-                readFileSync('release/wosim-hub-macos-x64.app.tar.gz.sig').toString(),
-            url: `https://github.com/wosim-net/hub/releases/download/v${process.env.INPUT_VERSION}/wosim-hub-macos-x64.app.tar.gz`
-        },
-        linux: {
-            signature:
-                readFileSync('release/wosim-hub-linux-amd64.AppImage.tar.gz.sig').toString(),
-            url: `https://github.com/wosim-net/hub/releases/download/v${process.env.INPUT_VERSION}/wosim-hub-linux-amd64.AppImage.tar.gz`
-        },
-        win64: {
-            signature:
-                readFileSync('release/wosim-hub-windows-x64.msi.zip.sig').toString(),
-            url: `https://github.com/wosim-net/hub/releases/download/v${process.env.INPUT_VERSION}/wosim-hub-windows-x64.msi.zip`
+if (!preRelease) {
+    const latest = {
+        version: process.env.INPUT_VERSION,
+        notes,
+        pub_date: new Date().toISOString(),
+        platforms: {
+            darwin: {
+                signature:
+                    readFileSync('release/wosim-hub-macos-x64.app.tar.gz.sig').toString(),
+                url: `https://github.com/wosim-net/hub/releases/download/v${process.env.INPUT_VERSION}/wosim-hub-macos-x64.app.tar.gz`
+            },
+            linux: {
+                signature:
+                    readFileSync('release/wosim-hub-linux-amd64.AppImage.tar.gz.sig').toString(),
+                url: `https://github.com/wosim-net/hub/releases/download/v${process.env.INPUT_VERSION}/wosim-hub-linux-amd64.AppImage.tar.gz`
+            },
+            win64: {
+                signature:
+                    readFileSync('release/wosim-hub-windows-x64.msi.zip.sig').toString(),
+                url: `https://github.com/wosim-net/hub/releases/download/v${process.env.INPUT_VERSION}/wosim-hub-windows-x64.msi.zip`
+            }
         }
     }
+
+    writeFileSync('static/latest.json', JSON.stringify(latest))
+    execSync('git add static/latest.json')
+
 }
 
-writeFileSync('static/latest.json', JSON.stringify(latest))
-execSync('git add static/latest.json')
 execSync('git add content')
 execSync(`git commit -m "chore: release ${tag}"`)
 execSync('git push')
